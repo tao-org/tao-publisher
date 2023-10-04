d53c568424dba51dd3be2161bd724cdd3ab7837f
Clément GUICHARD <clement.guichard@csgroup.eu>
2023-10-04 17:57:33 +0200

    refactor: create Application model for better Container validation

--- src/tao/models/container.py
+++ src/tao/models/container.py
@@ -1,6 +1,6 @@
 """TAO container models/schemas."""
 
-from typing import Any, List, Optional
+from typing import List, Optional
 
 from pydantic import BaseModel, Field
 
@@ -15,7 +15,7 @@ class Container(BaseModel):
     tag: str
     application_path: Optional[str] = Field(alias="applicationPath", default=None)
     logo: Optional[str] = Field(default=None)
-    applications: List[Any] = Field(default_factory=list)
+    applications: List["Application"] = Field(default_factory=list)
     format_: List[str] = Field(alias="format", default_factory=list)
     common_parameters: Optional[str] = Field(alias="commonParameters", default=None)
     format_name_parameter: Optional[str] = Field(
@@ -23,3 +23,15 @@ class Container(BaseModel):
         default=None,
     )
     owner_id: Optional[str] = Field(alias="ownerId", default=None)
+
+
+class Application(BaseModel):
+    """TAO Application model."""
+
+    path: str
+    name: str
+    memory_requirements: int = Field(alias="memoryRequirements")
+    parallel_flag_template: Optional[str] = Field(
+        alias="parallelFlagTemplate",
+        default=None,
+    )
