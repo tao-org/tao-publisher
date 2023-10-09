d49f6530aa8fbf1295f4e6c4428ed6e188ea09db
Clément GUICHARD <clement.guichard@csgroup.eu>
2023-10-09 15:26:40 +0200

    feat: add slugify to utils.http

--- src/tao/utils/http.py
+++ src/tao/utils/http.py
@@ -1,5 +1,6 @@
 """HTTP utilities."""
 
+import re
 from urllib.parse import urlparse
 
 HTTP_401_UNAUTHORIZED = 401
@@ -9,3 +10,12 @@ def is_uri(text: str) -> bool:
     """Return True if text is a valid URI, False otherwise."""
     parse_result = urlparse(text)
     return all([parse_result.scheme, parse_result.netloc])
+
+
+def slugify(text: str) -> str:
+    """Slugify string, URL-friendly and filename-friendly."""
+    text = text.lower().strip()
+    text = re.sub(r"[^\w\s-]", "", text)
+    text = re.sub(r"[\s_-]+", "-", text)
+    text = re.sub(r"^-+|-+$", "", text)
+    return text
