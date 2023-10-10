74a4b7d99b6cb5019f5f6c823fdfdda75314f3cb
Clément GUICHARD <clement.guichard@csgroup.eu>
2023-10-10 14:06:33 +0200

    feat: add file request serialization methods to utils.http

--- src/tao/utils/http.py
+++ src/tao/utils/http.py
@@ -1,21 +1,53 @@
 """HTTP utilities."""
 
+import mimetypes
 import re
+from pathlib import Path
+from typing import List, Optional, Tuple
 from urllib.parse import urlparse
 
 HTTP_401_UNAUTHORIZED = 401
 
+SerializedFile = Tuple[str, bytes, str]
 
-def is_uri(text: str) -> bool:
+
+def is_uri(text: str, /) -> bool:
     """Return True if text is a valid URI, False otherwise."""
     parse_result = urlparse(text)
     return all([parse_result.scheme, parse_result.netloc])
 
 
-def slugify(text: str) -> str:
+def slugify(text: str, /) -> str:
     """Slugify string, URL-friendly and filename-friendly."""
     text = text.lower().strip()
     text = re.sub(r"[^\w\s-]", "", text)
     text = re.sub(r"[\s_-]+", "-", text)
     text = re.sub(r"^-+|-+$", "", text)
     return text
+
+
+def serialize_files(files: List[Path], /, *, ctx_path: Path) -> List[SerializedFile]:
+    """Prepare files upload by transforming list of path to list of file data."""
+    files_data: List[SerializedFile] = []
+    for file_path in files:
+        file = serialize_file(file_path, ctx_path=ctx_path)
+        if file:
+            files_data.append(file)
+    return files_data
+
+
+def serialize_file(
+    file_path: Path,
+    /,
+    *,
+    ctx_path: Path,
+) -> Optional[SerializedFile]:
+    """Prepare file upload by reading path to file data."""
+    file_path = (ctx_path / file_path).resolve()
+    if file_path.is_file():
+        file_mimetype = mimetypes.guess_type(file_path)[0]
+        if not file_mimetype:
+            file_mimetype = "text/plain"
+        with file_path.open("rb") as f:
+            return (file_path.name, f.read(), file_mimetype)
+    return None
